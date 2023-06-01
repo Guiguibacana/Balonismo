f1cc1f0fd082f36d263ac1f196a3dccac1259e4d
rm86896 <guiguisilva1104@gmail.com>
2023-06-01 12:24:52 -0300

    Initial commit

---
+++ src/Pessoas.java
@@ -0,0 +1,37 @@
+    public class Pessoas {
+
+        public class Pessoa {
+
+            String nome;
+            String CPF;
+            String RG;
+            String telefone;
+            String telefoneContatoEmergencias;
+            String endereco;
+        }
+
+        public class Piloto {
+            String nome;
+            String CPF;
+            String numeroRegistroANAC;
+        }
+
+        public class Balao {
+            private String numeroIdentificacao;
+            private String registroRegularidade;
+        }
+
+        public class TermoCiencia {
+            private Pessoa pessoa;
+
+        }
+
+        public class VooBalao {
+            private Piloto piloto;
+            private Balao balao;
+            private TermoCiencia termoCiencia;
+            private double valorVoo;
+        }
+    }
+
+
